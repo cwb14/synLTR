f4f3ddc9a9673b250bd23ec53aadc6617abe6f68
Chris Benson <35149326+cwb14@users.noreply.github.com>
2025-12-19 18:24:06 -0500

    Add comments to ltrharvest.py for clarity
    Added comments for ltrharvest and TEsorter sections.

--- module2/ltrharvest.py
+++ module2/ltrharvest.py
@@ -22,13 +22,17 @@ Default parameters are benchmarked with PrinTE:
 
 - python ltrharvest.py --genome burnin.fasta --proteins PrinTE/data/TAIR10.pep.fa.gz --te-library lib_clean.fa --out-prefix myrun --threads 100
 
+# ltrharvest.
 - python ./PrinTE/util/bedtools.py -pass_scn myrun.ltrharvest.stitched.scn -bed <(cat burnin.bed | grep -v FRAG | grep -v gene | grep LTR) -r 0.90 -print unique-bed
   Overlapping entries: 1800 (1793 unique)
   Entries unique to SCN/PASS file: 8
   Entries unique to BED file: 6
 
-
-
+# ltrharvest + TEsorter.
+- python ./PrinTE/util/bedtools2.py -pass_scn <(cat myrun.ltrharvest.stitched.fa.rexdb-plant.cls.lib.fa | grep '>' | sed 's/^>//; s/#.*//') -bed <(cat burnin.bed | grep -v FRAG | grep -v gene | grep LTR) -r 0.90
+  Overlapping entries: 1658 (1651 unique)
+  Entries unique to SCN/PASS file: 4
+  Entries unique to BED file: 148
 """
 
 import argparse
